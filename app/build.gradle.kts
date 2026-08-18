94477842174369c29a26663ef6c1adb28782c52a
hglasswater-boop <h.glasswater@gmail.com>
2026-08-18 14:50:12 +0900

    Wire LibVLC into Android app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,6 +135,7 @@ dependencies {
 
     implementation(libs.media3.exoplayer)
     implementation(libs.media3.ui)
+    implementation(libs.libvlc)
 
     implementation(libs.shizuku.api)
     implementation(libs.shizuku.provider)
