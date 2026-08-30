0aff257e7bc82feaf32e7e0d4091c1e90a4d2e92
hglasswater-boop <h.glasswater@gmail.com>
2026-08-30 23:27:11 +0900

    Provide TV browser cast button stub

---
+++ app/src/tv/java/app/local1st/files/ui/main/EditionBrowserCastButton.kt
@@ -0,0 +1,8 @@
+package app.local1st.files.ui.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Suppress("UNUSED_PARAMETER")
+@Composable
+internal fun EditionBrowserCastButton(modifier: Modifier = Modifier) = Unit
