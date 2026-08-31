f743ce7981ad0f0a16bb8c0959c9fb0131c33362
hglasswater-boop <h.glasswater@gmail.com>
2026-08-31 22:18:24 +0900

    Opt in cast notification intent handler

--- app/src/mobile/java/app/local1st/files/EditionCastIntentHandler.kt
+++ app/src/mobile/java/app/local1st/files/EditionCastIntentHandler.kt
@@ -1,9 +1,11 @@
 package app.local1st.files
 
 import android.content.Intent
+import androidx.media3.common.util.UnstableApi
 import app.local1st.files.core.cast.CastPlaybackKeepAliveService
 import app.local1st.files.ui.viewer.CastPlaybackSessionManager
 
+@androidx.annotation.OptIn(UnstableApi::class)
 internal fun handleEditionIntent(intent: Intent): Boolean {
     if (intent.action != CastPlaybackKeepAliveService.ACTION_OPEN_PLAYBACK) return false
     CastPlaybackSessionManager.requestOpenControls()
